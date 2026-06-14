be5e05872bc5500e15c06b3d0e98ebafd2ab618a
Z E U S <iamdrzeus14@gmail.com>
2026-06-14 22:13:21 +0530

    adding fixes

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './', // This ensures your assets load correctly on GitHub Pages
+})

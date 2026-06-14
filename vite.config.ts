48a4343b89b62f051fb6cca59eae2ad2b5d40480
Z E U S <iamdrzeus14@gmail.com>
2026-06-14 22:52:01 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // This ensures your assets load correctly on GitHub Pages
+  base: '/just-bored/', // Using the explicit repo name fixes the path resolving issue
 })
